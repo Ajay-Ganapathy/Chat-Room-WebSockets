75baff11ac56e24bc87ecaf26a10794ec01bda2e
Ajay-Ganapathy <20epci002@skcet.ac.in>
2022-11-26 19:24:33 +0530

    update

--- server.js
+++ server.js
@@ -7,3 +7,18 @@ const port = 8000
 const server = http.createServer(express)
 const websocketserver = new WebSocket.Server({server})
 
+
+websocketserver.on('connection', function connection(ws) {
+  ws.on('message', function incoming(data) {
+    websocketserver.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(data);
+      }
+    })
+  })
+})
+
+
+server.listen(port, () => {
+  console.log(`Server is listening at Port ${port}`)
+})
